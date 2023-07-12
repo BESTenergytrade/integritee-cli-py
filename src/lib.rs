1c69adb90e4f158c47d3472f2d4cdba916cc0f3f
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-07-12 18:43:31 +0200

    successfully created a new account

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use pyo3::wrap_pyfunction;
 // Import the RPC client
 // use clap::Parser;
 use integritee_cli::{commands, Cli, trusted_cli::TrustedCli};
-use integritee_cli::commands::Commands;
+use integritee_cli::commands::{BaseCommand, Commands};
 use integritee_cli::trusted_cli::{PayAsBidCommand, TrustedBaseCommand, TrustedCommand};
 
 #[pymodule]
@@ -16,31 +16,52 @@ fn integritee_cli_py(_py: Python, m: &PyModule) -> PyResult<()> {
 #[pyfunction]
 fn run_cli(node_url: String, node_port: String, worker_url: String, trusted_worker_port: String, _command: String) -> PyResult<()> {
     println!("Run Cli");
-    let cli = pay_as_pid_cmd(node_url, node_port, worker_url, trusted_worker_port);
+
+    let cli = Cli {
+        node_url,
+        node_port,
+        worker_url,
+        trusted_worker_port,
+        command: new_account_cmd(),
+        // command: pay_as_bid_cmd(),
+    };
 
     commands::match_command(&cli).unwrap();
     Ok(())
 }
 
-fn pay_as_pid_cmd(node_url: String, node_port: String, worker_url: String, trusted_worker_port: String) -> Cli {
-    Cli {
-        node_url,
-        node_port,
-        worker_url,
-        trusted_worker_port,
-        command: Commands::Trusted(TrustedCli{
-            // random mrenclave, has to be replaced with one that has been fetched from the chain.
-            mrenclave: "4GMb72Acyg8hnnnGEJ89jZK5zxNC4LvSe2ME96wLRV6J".to_string(),
-            shard: None,
-            // signers and accounts starting with `//` will be recognized as dev-seeds and can
-            // always be used without first creating them in the keystore.
-            xt_signer: "//Alice".to_string(),
-            direct: false,
-            command: TrustedCommand::BaseTrusted(TrustedBaseCommand::PayAsBid(PayAsBidCommand {
-                account: "//Alice".to_string(),
-                orders_string: "".to_string(),
-            })),
-        }),
-    }
+/// Create a new account
+fn new_account_cmd() -> Commands {
+    Commands::Base(BaseCommand::NewAccount)
+}
+
+/// Create a new trusted account in the enclave.
+fn new_trusted_account_cmd() -> Commands {
+    Commands::Trusted(TrustedCli{
+        // random mrenclave, has to be replaced with one that has been fetched from the chain.
+        mrenclave: "4GMb72Acyg8hnnnGEJ89jZK5zxNC4LvSe2ME96wLRV6J".to_string(),
+        shard: None,
+        // signers and accounts starting with `//` will be recognized as dev-seeds and can
+        // always be used without first creating them in the keystore.
+        xt_signer: "//Alice".to_string(),
+        direct: false,
+        command: TrustedCommand::BaseTrusted(TrustedBaseCommand::NewAccount),
+    })
+}
+
+fn pay_as_bid_cmd() -> Commands {
+    Commands::Trusted(TrustedCli{
+        // random mrenclave, has to be replaced with one that has been fetched from the chain.
+        mrenclave: "4GMb72Acyg8hnnnGEJ89jZK5zxNC4LvSe2ME96wLRV6J".to_string(),
+        shard: None,
+        // signers and accounts starting with `//` will be recognized as dev-seeds and can
+        // always be used without first creating them in the keystore.
+        xt_signer: "//Alice".to_string(),
+        direct: false,
+        command: TrustedCommand::BaseTrusted(TrustedBaseCommand::PayAsBid(PayAsBidCommand {
+            account: "//Alice".to_string(),
+            orders_string: "".to_string(),
+        })),
+    })
 }
 
