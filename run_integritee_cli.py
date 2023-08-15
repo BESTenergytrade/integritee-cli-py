b9af30fe831593590decde3da65c41ad304a9065
m-yahya <yahya_gis@hotmail.com>
2023-08-15 16:30:51 +0200

    accept params for command

--- run_integritee_cli.py
+++ run_integritee_cli.py
@@ -9,12 +9,20 @@ if __name__ == "__main__":
         required=True,
         choices=[
             "new_account_cmd",
+            "new_trusted_account_cmd",
             "pay_as_bid_cmd",
             "get_market_results_cmd",
             "pay_as_bid_proof_cmd",
             "verify_proof_cmd",
         ],
         help="Specify the command to run",
+    )
+
+    parser.add_argument(
+        "--params",
+        nargs="+",  # Allows multiple arguments for params
+        required=True,  # Make the params argument required
+        help="Parameters for the command",
     )
 
     args = parser.parse_args()
@@ -26,8 +34,9 @@ if __name__ == "__main__":
 
     try:
         command_name = args.command
+        params = args.params
         integritee_cli_py.run_cli(
-            node_url, node_port, worker_url, trusted_worker_port, command_name
+            node_url, node_port, worker_url, trusted_worker_port, command_name, params
         )
     except Exception as e:
         print("Encountered an error:", e)
